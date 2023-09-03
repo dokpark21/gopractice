73551f4e43075a9df9ca06039daa44567552b4b5
BEB 09 박상현 <psh03225@naver.com>
2023-09-03 19:54:56 +0900

    Update : fmt package

--- package/fmt.go
+++ package/fmt.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
+var y = 42
+
 func main() {
 	n, e := fmt.Println("Hello golang", 42, true)
 	fmt.Println(n) //int 형식 출력 되는 바이트 실행
 	fmt.Println(e) //e는 err, 만약 err가 잡힌다면 출력
+
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)  //y type
+	fmt.Printf("%b\n", y)  //y binary(2진수)
+	fmt.Printf("%x\n", y)  //y 16진수
+	fmt.Printf("%#x\n", y) //y 0x16진수
+	//Println은 한줄 띄어서 출력, Printf는 포맷 프린트
+
+	s := fmt.Sprintf("%#x\t%b\t%x", y, y, y) //Sprint는 문자열 프린트
+	fmt.Println(s)
 }
 
 /*
