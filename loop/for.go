ba94924c33739f95675446e89cd81d0e5eb6b082
BEB 09 박상현 <psh03225@naver.com>
2023-09-05 11:15:18 +0900

    Add : loop for

---
+++ loop/for.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for i := 0; i <= 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// for k := 0; k <= 10; k++ {
+	// 	for j := 0; j <= 5; j++ {
+	// 		fmt.Println(k + j)
+	// 	}
+	// }
+
+	//for를 while처럼 사용하기
+	// x := 1
+	// for x < 10 {
+	// 	fmt.Println(x)
+	// 	x++
+	// }
+	// fmt.Println("done")
+
+	// y := 1
+	// for {
+	// 	if y < 10 {
+	// 		break
+	// 	}
+	// 	fmt.Println(y)
+	// 	y++
+	// }
+
+	y := 0
+	for {
+		y++
+		if y > 10 {
+			break
+		}
+		if y%2 == 0 {
+			continue
+		}
+		fmt.Println(y)
+
+	}
+}
