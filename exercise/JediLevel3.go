6c96f57bb72c171da99d5d3fcd3559c9912feef8
BEB 09 박상현 <psh03225@naver.com>
2023-09-07 15:39:10 +0900

    Add : exercise3

---
+++ exercise/JediLevel3.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+func main() {
+	//1
+	// for i := 1; i <= 1000; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	//2
+	// for i := 65; i <= 90; i++ {
+	// 	fmt.Println(i + 1)
+	// 	for k := 0; k < 3; k++ {
+	// 		// 유니코드 출력
+	// 		fmt.Printf("\t%#U\n", i)
+	// 	}
+	// }
+
+	//3
+	//for문을 while문 처럼 사용하기
+	// bd := 1999
+	// for bd < 2024 /* only condition */ {
+	// 	fmt.Println(bd)
+	// 	bd++
+	// }
+
+	//4
+	// bd := 1999
+	// for {
+	// 	if bd > 2023 {
+	// 		break
+	// 	} else {
+	// 		fmt.Println(bd)
+	// 		bd++
+	// 	}
+	// }
+
+	//5
+	// for i := 10; i <= 100; i++ {
+	// 	fmt.Println(i % 4)
+	// }
+
+	//6,7
+	// x := "James 1Bond"
+	// if x == "James Bond" {
+	// 	fmt.Println(x)
+	// } else if x == "James Bondie" {
+	// 	fmt.Println(x)
+	// } else {
+	// 	fmt.Println("Nothing found")
+	// }
+
+	//8
+	// switch {
+	// case false:
+	// 	fmt.Println("should not print")
+	// case true:
+	// 	fmt.Println("should print")
+	// }
+
+	//9
+	favSport := "basketball"
+	switch favSport {
+	case "soccer":
+		fmt.Println("your favorite sport is soccer!!")
+	case "basketball":
+		fmt.Println("your favorite sport is basketball")
+	}
+}
