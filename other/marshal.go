ae4c6bab9b444c8043834fb316bb14160e345838
BEB 09 박상현 <psh03225@naver.com>
2023-09-19 20:03:32 +0900

    Add : Unmarshal

--- other/marshal.go
+++ other/marshal.go
@@ -11,6 +11,9 @@ type person struct {
 	Age   int
 }
 
+// marshal : go type struct => JSON
+// unmarshal : JSON => go type struct(JSON으로 인코딩된 data를 분석하고 v가 가르키는 곳에 저장)
+
 func main() {
 	p1 := person{
 		First: "James",
@@ -27,7 +30,7 @@ func main() {
 	people := []person{p1, p2}
 	fmt.Println(people)
 
-	// json 형식으로 구조체를 marshall하기 위해서는 필드의 첫글자가 반드시 대문자여야 한다
+	// json 형식으로 구조체를 marshal하기 위해서는 필드의 첫글자가 반드시 대문자여야 한다
 	bs, err := json.Marshal(people)
 	if err != nil {
 		fmt.Println(err)
@@ -35,5 +38,13 @@ func main() {
 		// string으로 형변환해야 필드 명과 값이 프린트 된다
 		fmt.Println(string(bs))
 	}
-
+	// Unmarshal은 err를 반환하며 json을 다시 구조체로 바꾼 값을 저장하기 위한 바이트 슬라이스가 필요하다
+	var people2 []person
+	// Unmarshal (JSON형식의 값과 바이트 슬라이스 구조의 값을 저장할 곳이 필요하다)
+	err2 := json.Unmarshal(bs, &people2)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	fmt.Println(people2)
+	fmt.Println(people2[0].First)
 }
