b360821991153804a220243b5cc93cdd92818afa
BEB 09 박상현 <psh03225@naver.com>
2023-09-25 23:36:20 +0900

    Add : mutex

---
+++ concurrency/mutex.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs : ", runtime.NumCPU())
+	fmt.Println("Go routines : ", runtime.NumGoroutine())
+
+	counter := 0
+	var wg sync.WaitGroup
+	const gs = 100
+	wg.Add(gs)
+
+	var mu sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			// 코드를 잠궈서 counter 변수에 아무도 접근하지 못하게한다.
+			// v++ -> counter = v로 넘어가는 과정에서 만약 다른 go routine이  v:= counter를 실행하게 되면서 값이 늘어난 것이 다시 원상태로 돌아온 것이다.
+			mu.Lock()
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			// 동작이 끝나면 잠금을 해제한다.
+			mu.Unlock()
+			wg.Done()
+		}()
+		fmt.Println("Go routines : ", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Go routines : ", runtime.NumGoroutine())
+	fmt.Println("Counter : ", counter)
+}
---
+++ concurrency/race_condition.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+// 아래의 코드를 실행시켜 보면 go routine의 개수가 오르락 내리락하고 Counter도 100이 아닌 숫자가 계속 바뀔 것이다.
+// 이런 이유는 경쟁상태에 있어서이다
+
+func main() {
+	fmt.Println("CPUs : ", runtime.NumCPU())
+	fmt.Println("Go routines : ", runtime.NumGoroutine())
+
+	counter := 0
+	var wg sync.WaitGroup
+	const gs = 100
+	wg.Add(gs)
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			// 1초 기다린다
+			// time.Sleep(time.Second)
+			// 다른 go routine에게 양보(yield)
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Go routines : ", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Go routines : ", runtime.NumGoroutine())
+	fmt.Println("Counter : ", counter)
+}
